46da34ad5b5a7d989efcefc71a21fe573669bd92
Vlad <vlad220994@gmail.com>
2019-07-12 20:45:41 +0300

    create store

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import { createStore } from "redux";
+
 const reducer = (state = 0, action) => {
 
   switch(action.type) {
@@ -10,11 +12,12 @@ const reducer = (state = 0, action) => {
   }
 }
 
-let state = reducer(undefined, {});
-console.log(state);
+const store = createStore(reducer);
 
-state = reducer(state, {type: "INC"});
-console.log(state);
+store.subscribe(() => {
+  console.log(store.getState());
+});
 
-state = reducer(state, { type: "DEC" });
-console.log(state);
+store.dispatch({ type: "INC" });
+store.dispatch({ type: "INC" });
+store.dispatch({ type: "INC" });
