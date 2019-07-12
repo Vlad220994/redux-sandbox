9303a1473217dd07e55e5ab0804c6802a9581155
Vlad <vlad220994@gmail.com>
2019-07-13 00:14:35 +0300

    project structure changed

--- src/index.js
+++ src/index.js
@@ -1,31 +1,28 @@
 import { createStore } from "redux";
-
-const reducer = (state = 0, action) => {
-
-  switch(action.type) {
-    case "INC":
-      return state + 1;
-    case "DEC":
-      return state - 1;
-    default: 
-      return state;
-  }
-}
+import reducer from "./reducer";
+import { inc, dec, rnd } from "./actions";
 
 const store = createStore(reducer);
 
 document
   .getElementById("inc")
   .addEventListener("click", () => {
-    store.dispatch({ type: "INC" })
+    store.dispatch(inc());
   });
 
 document
   .getElementById("dec")
   .addEventListener("click", () => {
-    store.dispatch({ type: "DEC" })
+    store.dispatch(dec());
   });
 
+document
+  .getElementById("rnd")
+  .addEventListener("click", () => {
+    const payload = Math.floor(Math.random()*10);
+    store.dispatch(rnd(payload));
+  });
+ 
 const update = () => {
   document
     .getElementById("counter")
